2661249dcf10fddc790dc4efbac5d9130934f81b
KoknePranit <pranit.k@turtlemint.com>
2020-03-07 22:01:35 +0530

    changing the compiler source plugin to 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,25 @@
 package clinic.programming.training;
 
+import java.util.*;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
 
+	public void greet(){
+	List<String> greetings = new ArrayList<>();
+	greetings.add("Hello");
+	for(String msg : greetings){
+	System.out.println(msg);
+}
+}
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
     }
 }
